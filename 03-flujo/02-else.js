63d4ac492d38a20f4856dc5b180b50ac59fc5ad6
Axel Darat <axeldarat@gmail.com>
2025-12-01 17:31:37 -0300

    flujo

---
+++ 03-flujo/02-else.js
@@ -0,0 +1,9 @@
+let edad = 12;
+
+if (edad > 17) {
+    console.log('Usuario mayor de edad. Puede entrar.');
+} else if (edad > 13) {
+    console.log('Usuario necesita estar acompañado de sus padres');
+} else {
+    console.log('Usuario menor de edad. No puede entrar.');
+}
