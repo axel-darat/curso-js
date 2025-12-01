084875dfd386d4d078d7e91c1066d46531b02446
Axel Darat <axeldarat@gmail.com>
2025-11-30 23:39:12 -0300

    operadores

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+let a = 10;
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+// no se utilizan estos símbolos de igualdad
+console.log(a == '10');
+console.log(a != '10');
+
+// se utilizan ESTOS!
+console.log(a === '10');
+console.log(a !== '10');
