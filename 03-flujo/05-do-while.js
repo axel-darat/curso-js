36c0145b7955603e239e50f08def64c4424d8c70
Axel Darat <axeldarat@gmail.com>
2025-12-04 21:48:00 -0300

    10

---
+++ 03-flujo/05-do-while.js
@@ -0,0 +1,16 @@
+// Impresión de números pares
+
+let i = 0;
+// while (i < 3) {
+//     if (i % 2 == 0) {
+//         console.log('Número par', i);
+//     }
+//     i++;
+// }
+
+do {
+    if (i % 2 == 0) {
+        console.log('Número par', i);
+    }
+    i++;
+} while (i < 2);
