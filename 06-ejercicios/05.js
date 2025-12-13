8228b762a1e50c090cb0f2798ca251cc8e36a721
axel <axeldarat@gmail.com>
2025-12-12 21:37:48 -0300

    ejercicios

--- 06-ejercicios/05.js
+++ 06-ejercicios/05.js
@@ -10,9 +10,12 @@ let propiedad = 'name';
 function tieneProp(obj, propiedad) {
     let props = Object.keys(obj);
 
-    for (let prop in props) {
-        
+    for (let prop of props) {
+        if (propiedad == prop) {
+            return true;
+        }
     }
+    return false;
 }
 
 console.log(tieneProp(objeto, propiedad));
