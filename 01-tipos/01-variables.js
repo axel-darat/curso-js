0e5077088a5cfc07033f09dc8dfc1e9a7c42b07b
Axel Darat <axeldarat@gmail.com>
2026-01-03 17:31:55 -0300

    tipos

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,16 @@
+// UpperCamelCase, camelCase, snake_case
+
+let nombre = 'Hola Mundo!';
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+// Siempre debe comenzar con letras
+// let 1nth;
+
+console.log(nombre);
+
+let apellido;
+let animal;
+apellido = "Darat";
+console.log(apellido);
