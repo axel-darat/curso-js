4553585ccc4940625136d59867b29eb5225f5d0d
axel <axeldarat@gmail.com>
2026-01-15 01:08:58 -0300

    tipos

--- 01-tipos/07-arrays.js
+++ 01-tipos/07-arrays.js
@@ -1,1 +1,11 @@
-let animales = 
+let animales = ['chanchito', 'caballo'];
+
+console.log(animales);
+console.log(animales[0]);
+animales[2] = 'dragón';
+console.log(animales);
+
+animales[10] = 'Pez';
+console.log(animales);
+console.log(typeof animales);
+animales.length;
