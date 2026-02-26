eb45da1b22ceeafe416939586f2ddf6ac36416b5
Axel Darat González <adarat@inacap.cl>
2026-02-26 14:36:33 -0300

    operadores

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+let a = 10;
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+// NO USAR. SÓLO VALIDA DATO
+console.log(a == 10);
+console.log(a != 10);
+
+// USAR ESTO. VALIDA DATO Y TIPO
+console.log(a === '10');
+console.log(a !== '10');
