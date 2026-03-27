5189ec62aa6764092924cea028a9b17b0e896203
Axel Darat <axeldarat@gmail.com>
2026-03-26 22:36:13 -0300

    tipos

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,8 @@
+// UpperCamelCase, camelCase, snake_case
+let nombre;
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+// * Las variables deben ser autoexplicativas
+console.log(nombre);
