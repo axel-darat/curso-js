cd61eb904b6dc8a3d4f79ef9b724ca35603e95e9
Axel Darat <axeldarat@gmail.com>
2026-03-30 12:39:01 -0300

    tipos

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,18 @@
+// comentario de una línea
+
+/*
+Comentario
+multilinea
+oficial
+según ECMAScript
+*/
+
+/**
+ * Este es un comentario
+ * de muchas
+ * líneas
+ */
+
+let numero = 42; // otro comentario
+
+console.log(numero);
