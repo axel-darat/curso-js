fe63fa1618179e0b012881ed9137a60f59f4f032
Axel Darat <axeldarat@gmail.com>
2026-03-31 13:30:50 -0300

    control flujo

---
+++ 03-control-flujo/02-else.js
@@ -0,0 +1,9 @@
+let edad = 10;
+
+if (edad > 17) {
+    console.log('Usuario mayor de edad');
+} else if (edad > 13) {
+    console.log('Debe ingresar junto a sus tutores legales');
+} else {
+   console.log('Usuario menor de edad. No puede ingresar.');
+}
---
+++ 03-control-flujo/05-do-while.js
@@ -0,0 +1,19 @@
+// cuales son los números pares
+
+let i = 2;
+// while (i < 2) {
+//     if (i % 2 == 0) {
+//         console.log('Número par', i);
+//     }
+//     i++;
+// }
+
+do {
+    if (i % 2 == 0) {
+        console.log('Número par', i);
+    }
+    i++;
+} while (i < 2);
+
+// IMPRIME CUANDO ES NECESARIO QUE EL BLOQUE SE EJECUTE
+// AL MENOS UNA VEZ
