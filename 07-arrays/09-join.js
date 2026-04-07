633f0b5f8eff678dc6ae44dad773a55a8d9313d3
Axel Darat <axeldarat@gmail.com>
2026-04-07 13:20:33 -0400

    arrays

---
+++ 07-arrays/09-join.js
@@ -0,0 +1,10 @@
+let arr1 = ['Nicolas', 'Chanchito', 'Felipe'];
+
+let mensaje = arr1.join();
+
+console.log(mensaje);
+
+let saludo = "Hola Mundo! desde Chile!"
+let dividido = saludo.split(" ");
+
+console.log(dividido.join("-"));
