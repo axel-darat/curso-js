b90563edb362218883ab0f20f8abbc8bcef70728
Axel Darat <axeldarat@gmail.com>
2026-04-08 22:01:54 -0400

    ejercicios

---
+++ 08-ejercicios/02.js
@@ -0,0 +1,16 @@
+const miArray = [
+    "Hola",
+    12,
+    "Mundo",
+    {},
+    { id: 15 },
+    ['lala'],
+]
+
+// numeros, strings, objetos
+function dividePorTipo(arr) {
+
+}
+
+const nuevoArray = dividePorTipo(miArray);
+console.log(nuevoArray);
