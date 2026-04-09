dec2f7ae82b08071215cf03cd989880218c6dd9f
Axel Darat <axeldarat@gmail.com>
2026-04-09 13:13:21 -0400

    ejercicios

--- 08-ejercicios/02.js
+++ 08-ejercicios/02.js
@@ -9,7 +9,11 @@ const miArray = [
 
 // numeros, strings, objetos
 function dividePorTipo(arr) {
-
+    return {
+        numeros: arr.filter(n => typeof n === "number"),
+        strings: arr.filter(n => typeof n === "string"),
+        objetos: arr.filter(n => typeof n === "object"),
+    }
 }
 
 const nuevoArray = dividePorTipo(miArray);
