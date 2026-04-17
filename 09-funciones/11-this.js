3678c1bed97774ae8b0956c4329b93e1214d5c30
Axel Darat <axeldarat@gmail.com>
2026-04-16 21:43:43 -0400

    funciones

--- 09-funciones/11-this.js
+++ 09-funciones/11-this.js
@@ -1,17 +1,35 @@
-// this hace referencia a un objeto
-// en una func hace referencia a objeto window, global
-// en new hace referencia a un objeto que será creado
+// Dentro de un objeto: this hace referencia a un objeto
+// En una función: this hace referencia a obj window, global
+// en "new": hace referencia al objeto que será creado
 
-const user = {
-    name: 'Nicolas',
-    login() {
-        console.log(this);
-    }
-};
+// const user = {
+//     name: 'Nicolas',
+//     login() {
+//         console.log(this);
+//     }
+// };
 
-user.logout = function () {
-    console.log(this);
-}
+// user.logout = function logout() {
+//     console.log(this);
+// }
 
 // user.login();
-user.logout();
+
+// function log() {
+//     console.log(this);
+// }
+
+// log();
+
+function Log(mensaje) {
+    this.mensaje = mensaje;
+    console.log(this);
+}
+/**
+ * 1. Se crea un objeto literal
+ * 2. Se vincula este objeto a this
+ * 3. Se vincula a un prototipo
+ * 4. Si no retorna nada, retorna this
+ */
+
+const l = new Log('Hola Mundo');
