e401b4b057e52be70b845ed45fe256b48a26c624
Axel Darat <axeldarat@gmail.com>
2026-04-21 19:43:34 -0400

    operadores

---
+++ 02-operadores/06-bitwise.js
@@ -0,0 +1,24 @@
+// Decimal: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+// Binario: 0, 1
+// bit: 0 1
+
+// Byte: 8 bits
+// Byte: 00000000 -> 0
+// Byte: 00000001 -> 1
+// Byte: 00000010 -> 2
+// Byte: 00000011 -> 3
+// Byte: 00000100 -> 4
+// Byte: 00000101 -> 5
+// Byte: 00000110 -> 6
+// Byte: 00000111 -> 7
+
+
+// OR Realiza suma de bits
+console.log(1 | 3); // 3 en Byte
+console.log(1 | 4); // 5 en Byte
+console.log(3 | 5); // 7 en Byte
+
+// AND Verifica si se comparten 1
+console.log(1 & 3); // 1 en Byte
+console.log(1 & 4); // 0 en Byte
+console.log(3 & 5); // 1 en Byte
