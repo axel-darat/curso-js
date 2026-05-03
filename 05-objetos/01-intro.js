2194f541833a277fa5089eab34e5dc3489a81008
Axel Darat <axeldarat@gmail.com>
2026-05-03 13:44:35 -0400

    objetos

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,19 @@
+let email = 'axel@darat.com';
+let name = 'Axel';
+let direccion = {
+    calle: 'Calle Falsa',
+    numero: 123,
+}
+
+let user = {
+    email: 'axel@darat.com',
+    name: 'Axel',
+    direccion: {
+        calle: 'Calle Falsa',
+        numero: 15,
+    },
+    activo: true,
+    recuperarClave: function () {
+        console.log('Recuperando clave...');
+    },
+};
