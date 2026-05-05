e19a06f85edee4ce20383e84feb2fb4a891b7c31
Axel Darat <axeldarat@gmail.com>
2026-05-05 12:35:15 -0400

    objetos

---
+++ 05-objetos/13-date.js
@@ -0,0 +1,15 @@
+const ahora = new Date();
+// console.log(ahora);
+
+// MES DIA AÑO
+const fecha = new Date('December 11 1986 14:15 GMT-0300');
+// console.log(fecha);
+
+const fecha2 = new Date(1986, 11, 25, 14, 15);
+const fecha3 = new Date(1986, 11, 25, 14+15, 15);
+// console.log(fecha2);
+// console.log(fecha3);
+
+console.log('DateString:', fecha3.toDateString());
+console.log('ISOString:', fecha3.toISOString());
+console.log('TimeString:', fecha3.toTimeString());
