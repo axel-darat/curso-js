9bd8d403b6a1a6f6746a05068e0e67612c025d3a
Axel Darat <axeldarat@gmail.com>
2026-05-06 18:29:07 -0400

    objetos

---
+++ 05-objetos/15-escape.js
@@ -0,0 +1,11 @@
+/**
+ * \n nueva linea
+ * \t tabulación
+ * \' comilla simple
+ * \" comilla doble
+ * 
+ */
+const saludo = "Hola \\ mundo! \n Bienvenidos a \"ultimate javascript\""
+
+
+console.log(saludo);
---
+++ 05-objetos/16-template.js
@@ -0,0 +1,18 @@
+let user = "Axel";
+const saludo = `
+Hola mundo!
+
+Bienvenidos a "Ultimate JS"
+
+jeje xd
+
+e
+p
+i
+c
+o
+
+mi nombre es ${user}
+`;
+
+console.log(saludo);
