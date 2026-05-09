28a60f2bde32cfe18c7c49a6c004e55c50bb6aa2
Axel Darat <axeldarat@gmail.com>
2026-05-08 21:57:41 -0400

    arrays

---
+++ 07-arrays/03-buscar-primitivos.js
@@ -0,0 +1,12 @@
+const letras = ['a', 'b', 'c', 1, 'd', 1];
+
+console.log(letras.indexOf('c'));
+console.log(letras.indexOf(1, 3));
+console.log(letras.indexOf(1));
+console.log(letras.indexOf(2));
+console.log(letras.lastIndexOf(1));
+
+console.log(letras.indexOf(1) !== -1);
+
+console.log(letras.includes('d'));
+
