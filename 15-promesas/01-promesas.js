abd2374ba4cf1b3298fdaee993bf36a851e6d568
Axel Darat <axeldarat@gmail.com>
2026-07-06 22:35:04 -0400

    promesas

---
+++ 15-promesas/01-promesas.js
@@ -0,0 +1,7 @@
+let promesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('hola!'), 1000);
+});
+
+promesa.then(valor => console.log(valor),
+    e => console.log('error', e),
+);
