6735ddd817c14677460577ff7af318fe4e528a9b
Axel Darat <axeldarat@gmail.com>
2026-07-10 12:32:35 -0400

    operadores

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,19 @@
+let a = 5;
+let b = 7;
+
+console.log('adición', a + b);
+console.log('resta', a - b);
+console.log('multiplicación', a * b);
+console.log('división', a / b);
+console.log('módulo', a % b);
+console.log('potencia', a ** b);
+
+// incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+
+// decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,12 @@
+let a = 10;
+
+// console.log(a > 5);
+// console.log(a >= 5);
+// console.log(a < 5);
+// console.log(a <= 5);
+
+console.log(a == 10);
+console.log(a != 10);
+console.log(a == '10'); // evalua valor no tipo
+console.log(a === '10'); // evalua valor y tipo
+
