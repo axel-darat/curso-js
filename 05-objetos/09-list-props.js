3ebc5016e140806350c7a5a15308ea54c1a3885e
Axel Darat <axeldarat@gmail.com>
2026-07-13 10:18:47 -0400

    objetos

--- 05-objetos/09-list-props.js
+++ 05-objetos/09-list-props.js
@@ -2,15 +2,24 @@ const punto = {
     x: 10,
     y: 15,
     dibujar: function() {
-        console.log('dibujando');
+        console.log('Dibujando');
     }
 };
 
-delete punto.dibujar;
-if ('dibujar' in punto) {
+// delete punto.dibujar;
+if('dibujar' in punto) {
     punto.dibujar();
-} else {
-    console.log('No se encuentra propiedad "dibujar"');
 }
 
-console.log(Object.keys(punto));
+// console.log(Object.keys(punto));
+for (let llave of Object.keys(punto)) {
+    console.log(llave, punto[llave]);
+}
+
+for (let entry of Object.entries(punto)) {
+    console.log(entry);
+}
+
+for (let llave in punto) {
+    console.log(llave, punto[llave]);
+}
---
+++ 05-objetos/11-private-public.js
@@ -0,0 +1,15 @@
+function Usuario() {
+    this.name = 'Axel';
+
+    let log = function () {
+        console.log('logging...');
+    }
+
+    this.guardar = function () {
+        log();
+        console.log('guardando...');
+    }
+}
+
+const usuario = new Usuario();
+usuario.guardar();
