8be2be613d526e3f58b3592da4a07bc9642e8a65
Axel Darat <axeldarat@gmail.com>
2026-08-02 11:14:42 -0400

    herencia

---
+++ 12-herencia/03-method-override.js
@@ -0,0 +1,24 @@
+function getProto(u) {
+    return Object.getPrototypeOf(u);
+}
+
+function Entidad() {}
+
+Entidad.prototype.save = function () {
+    console.log('guardando desde entidad...');
+}
+
+function User() {}
+
+// Method override
+User.prototype.save = function () {
+    console.log('guardando desde User');
+}
+
+function Product() {}
+function Category() {}
+
+Object.setPrototypeOf(User.prototype, Entidad.prototype);
+
+const u = new User();
+console.log(getProto(getProto(u)).save());
